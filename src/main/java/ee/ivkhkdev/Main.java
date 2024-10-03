f4d063fa5253a183a88cbff5482c4f0600f9758e
becubie <dimamakaronkin@gmail.com>
2024-10-03 09:23:18 +0300

    JPTV23Task1DmitriMakarov

--- src/main/java/ee/ivkhkdev/Main.java
+++ src/main/java/ee/ivkhkdev/Main.java
@@ -2,83 +2,8 @@ package ee.ivkhkdev;
 import java.util.Random;
 import java.util.Arrays;
 import java.util.Scanner;
-// Exerscise 1
 public class Main {
-//    public static void main(String[] args) {
-//        System.out.printl("Java Hello!")
-//    }
-//}
-
-    // Exercise 2
-//    static int SumNum;
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter 1st 3-digits number: ");
-//        int num1 = scanner.nextInt();
-//        System.out.println("Enter 2nd 3-digits number: ");
-//        int num2 = scanner.nextInt();
-//        int SumNum = num1+num2;
-//        System.out.println("Your 3-digits number sum is: " + SumNum);
-//    }
-// Exercise 3
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Your name: ");
-//        String name = scanner.nextLine();
-//        System.out.printf("Your surname: ");
-//        String surname = scanner.nextLine();
-//        System.out.printf("Your BirthMonth (Letters): ");
-//        String BMonth = scanner.nextLine();
-//        System.out.printf("Your birthDay: ");
-//        int BDay = scanner.nextInt();
-//        System.out.printf("Your birthYear: ");
-//        int BYear = scanner.nextInt();
-//        System.out.println(name + " " + surname + " born in " + BDay + ". " + BMonth + " " + BYear + " year.");
-//
-//
-//    }
-// Exercise 4
-//    private int randomnum;
-//    public static void main(String[] args) {
-//        Random random = new Random();
-//        int[] randomnum = new int[100];
-//        int sum = 0;
-//        for(int i = 0; i < 100; ++i) {
-//            int num = random.nextInt(99);
-//            randomnum[i] = num;
-//            sum += num;
-//            System.out.println(num);
-//        }
-//        double average = (double)sum / (double)randomnum.length;
-//        System.out.println("Average Arithmetic is " + average);
-//    }
-// Exercise 5
-//    public static void main(String[] args) {
-//        int[][] jaggedArray = new int[5][];
-//        jaggedArray[0] = new int[3];
-//        jaggedArray[1] = new int[5];
-//        jaggedArray[2] = new int[7];
-//        jaggedArray[3] = new int[8];
-//        jaggedArray[4] = new int[9];
-//        Random rand = new Random();
-//        for (int i = 0; i < jaggedArray.length; i++) {
-//            for (int j = 0; j < jaggedArray[i].length; j++) {
-//                jaggedArray[i][j] = rand.nextInt(51) + 50;
-//                }
-//            }
-//            System.out.println("Sorted: ");
-//            printJaggedArray(jaggedArray);
-//            for (int i = 0; i < jaggedArray.length; i++) {
-//                Arrays.sort(jaggedArray[i]);
-//            }
-//            System.out.println("\nUnsorted: ");
-//            printJaggedArray(jaggedArray);
-//        }
-//        public static void printJaggedArray(int[][] array) {
-//            for (int[] row : array) {
-//                System.out.println(Arrays.toString(row));
-//            }
-//        }
+    public static void main(String[] args) {
+        System.out.printl("Java Hello!")
     }
-
-
+}
